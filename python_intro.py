f2f252090894d39f4ef1acce1268e5d9096875c5
Anke <anke@example.com>
2016-04-06 16:21:00 +0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,10 @@
+def hi(name):
+    print ('Hallon '+ name+'!')
+
+cats = ['Herman', 'Pompoen', 'Roosje', 'Neeg']
+for name in cats:
+    hi(name)
+    print('Volgende kat')
+
+for i in range(1,7):
+    print(i)
